76e4fafdc7fb1525256488916d8846d0e3aac3a7
douguizilla <douglasgommez@gmail.com>
2021-10-03 13:41:08 -0300

    created intent to call ViewAnimationsActivity

--- app/src/main/java/com/odougle/animacoes/MainActivity.kt
+++ app/src/main/java/com/odougle/animacoes/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.odougle.animacoes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.odougle.animacoes.databinding.ActivityMainBinding
@@ -11,5 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val intent = Intent(this, ViewAnimationsActivity::class.java)
+        startActivity(intent)
     }
 }
